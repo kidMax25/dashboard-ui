716136e55e4ba1701cd8267165dad1cdac5352a5
kidMax25 <snmaxis@gmail.com>
2024-06-16 16:01:02 +0300

    Final

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-[100vh] flex items-center justify-center relative">
     {children}
-    <div className="absolute bottom-5 rigth-0"><ThemeToggler/></div>
+    <div className="absolute bottom-5 rigth-0 text-white"><ThemeToggler/></div>
     </div>
   );
 };
